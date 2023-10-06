397c658f0aa8a3cc4972cbda698296edfc3c8786
Peter Chahid <peterdaaboul@outlook.com>
2023-10-06 19:42:57 +0300

    basic commands

---
+++ OSLAB04/Problem1.c
@@ -0,0 +1,50 @@
+// -Step 1: 
+// mkdir OS
+
+//-Step 2:
+// cd OS
+
+//-Step 3:
+// mkdir Dir1 Dir2
+
+//-Step 4a:
+// cd Dir1; nano hello.txt
+
+// -Step 4b:
+//-"Hello World!" to hello.txt,  Ctrl+X, Y, Enter 
+
+// -Step 4a:
+// nano file.txt
+
+//-Step 4d:
+// "Nothing Important:)",  Ctrl+X, Y, 
+
+//-Step 5: 
+// cp hello.txt ../Dir2/copied.txt
+
+//-Step 6:
+// ls ../Dir2
+
+//-Step 7:
+// cat file.txt >> hello.txt
+
+//Step 8:
+// ls -l
+
+//-Step 9:
+// cat hello.txt
+
+//-Step 10:
+// rm hello.txt
+
+//-Step 11:
+// cd ..
+
+//-Step 12:
+// pwd
+
+// -Step 13: 
+// rmdir Dir1
+
+//-Step 14:
+// rm Dir2/copied.txt; rmdir Dir2
